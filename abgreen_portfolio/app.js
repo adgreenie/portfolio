8eeb4dfae38310b1dd381fcc056948657048d2ef
Adam Green <adgreenie@gmail.com>
2020-03-11 21:43:49 -0400

    fixed nav/scroll issues

--- abgreen_portfolio/app.js
+++ abgreen_portfolio/app.js
@@ -13,28 +13,24 @@ const pages = ['home','about','project','contact']
 
 let $navList = $('<ul>').addClass('navbar-nav mr-auto')
 
+let $previous = $('.home-page')
+
 pages.forEach((page, i) => {
     let $li = $(`<li class="nav-item">
                     <a class="nav-link" href="#${page}">${page}</a>
                 </li>`)
     $li.on('click', function() {
-        $('main').css('left',`${-i*100}vw`)
-        $(`.${page}-page`).removeClass('off-screen')
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+        $('main').css('left', `${-i*100}vw`)
+        setTimeout(() => {
+            $(`.${page}-page`).removeClass('off-screen')
+        }, 800)
+        $previous.addClass('off-screen')
+        $previous = $(`.${page}-page`)
     })
     $navList.append($li)
 })
 $('#navbarSupportedContent').append($navList)
-
-let $previous = $('.home-page')
-
-$(window).scroll(() => {
-    if($(window).scrollTop() + $(window).height() === $(document).height()) {
-        $('main').addClass('screen-scroll')
-        $('.project-page').removeClass('off-screen')
-        $(window).scrollTop(0)
-    }
- })
-
-$(window).on('beforeunload', function() {
-    $(window).scrollTop(0)
-}); // page will load at top on refresh
